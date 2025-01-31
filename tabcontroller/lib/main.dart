ecb36c092d3ef7539ad6b9b8208a1f57a8312dc4
Jesus Herrera <jesusherrera1594@gmail.com>
2025-01-31 15:47:01 -0600

    add tab controller app

---
+++ tabcontroller/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'screens/home.dart';
+import 'screens/cart.dart';
+import 'screens/profile.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: Profile(),
+      ),
+    );
+  }
+}

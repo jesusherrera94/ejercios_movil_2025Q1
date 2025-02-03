0b232f26074d1712a02d9cccd2b92b7d411a4528
Jesus Herrera <jesusherrera1594@gmail.com>
2025-02-02 18:50:41 -0600

    add menu drawer

--- menudrawer/lib/main.dart
+++ menudrawer/lib/main.dart
@@ -9,6 +9,8 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,9 +19,89 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        body: Profile(),
-      ),
+      home: const Menu()
     );
   }
+}
+
+// ================================== Menu =====================================
+
+class Menu extends StatefulWidget {
+  const Menu({super.key});
+
+  
+  @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+
+  int _selectedIndex = 1;
+
+  final  List<Widget> _screens = [
+   const Home(),
+    const Cart(),
+    const Profile()
+  ];
+
+  void _selectItem (int newIndex) {
+    setState(() {
+      _selectedIndex = newIndex;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('My super app!'),
+          leading: Builder(builder: (context) {
+          return IconButton(onPressed: () {
+            // abre el drawer
+            Scaffold.of(context).openDrawer();
+          }, icon: const Icon(Icons.menu));
+        }),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                child: Text("Menu header"),
+                decoration: BoxDecoration( color: Colors.blue),
+              ),
+              ListTile(
+                title: const Text("Home"),
+                selected: _selectedIndex == 1,
+                onTap: () {
+                  // TODO: actualizar estado
+                  _selectItem(1);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("Cart"),
+                selected: _selectedIndex == 2,
+                onTap: () {
+                  // TODO: actualizar estado
+                  _selectItem(2);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("Profile"),
+                selected: _selectedIndex == 3,
+                onTap: () {
+                  // TODO: actualizar estado
+                  _selectItem(3);
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          ),
+        ),
+        body: _screens[_selectedIndex - 1],
+      );
+  }
+  
 }

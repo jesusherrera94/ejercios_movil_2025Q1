9088f2dc0853bc347261c961ee77d70123655a71
Jesus Herrera <jesusherrera1594@gmail.com>
2025-02-12 20:57:21 -0600

    complete gridview, product_item and listview

--- layouts4/lib/screens/home.dart
+++ layouts4/lib/screens/home.dart
@@ -16,61 +16,21 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return GridView.count(
       padding: const EdgeInsets.all(20),
-      crossAxisCount: 2,
+      crossAxisCount: 1,
       crossAxisSpacing: 10,
       mainAxisSpacing: 10,
+      childAspectRatio: 2,
       children: [
         ProductItem(),
-        Container(
-        padding: const EdgeInsets.all(8),
-        color: Colors.teal[200],
-        child: const Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non lacus magna. Mauris at hendrerit elit. Etiam vehicula accumsan placerat. Nulla at diam id nunc laoreet blandit sed nec nisl. Integer finibus massa molestie elit pellentesque, vitae accumsan risus porta. In aliquam ante non felis accumsan imperdiet. Etiam quis vulputate arcu, at pulvinar diam. '),
-      ),
-        Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[200],
-      child: const Text('Heed not the rabble'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[300],
-      child: const Text('Sound of screams but the'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[400],
-      child: const Text('Who scream'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[500],
-      child: const Text('Revolution is coming...'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[600],
-      child: const Text('Revolution, they...'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[300],
-      child: const Text('Sound of screams but the'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[400],
-      child: const Text('Who scream'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[500],
-      child: const Text('Revolution is coming...'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[600],
-      child: const Text('Revolution, they...'),
-    ),
+        ProductItem(),
+        ProductItem(),
+        ProductItem(),
+        ProductItem(),
+        ProductItem(),
+        ProductItem(),
+        ProductItem(),
+        ProductItem(),
+        ProductItem(),
       ],
     );
   }
--- layouts4/lib/screens/product_item.dart
+++ layouts4/lib/screens/product_item.dart
@@ -11,7 +11,46 @@ class _ProductItemState extends State<ProductItem> {
   Widget build(Object context) {
     return Container(
       decoration: BoxDecoration(border: Border.all(color: Colors.blueGrey)),
-      padding: const EdgeInsets.all(8),
+      padding: const EdgeInsets.only(left: 8, right: 8, top: 4, bottom: 4),
+      child: Row(
+        children: [
+          Image(image: AssetImage('assets/img/guitar.jpg.auto.webp'),
+            width: 80,
+            height: 200,
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) {
+              return const Icon(Icons.error);
+            },
+          ),
+          const SizedBox(width: 8,),
+          Expanded(child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Gibson SG",
+                style: TextStyle( fontWeight: FontWeight.bold),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+              Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non lacus magna.",
+                style: TextStyle(fontSize: 12),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+
+              Text("\$99.99",
+                style: TextStyle(fontSize: 14, color: Colors.green, fontWeight: FontWeight.bold, ),
+              ),
+              Center(
+                child: ElevatedButton(onPressed: () {
+                print("item added to cart");
+              }, child: const Text("Add to cart"),
+              ),
+              )
+            ],
+          ))
+        ],
+      ),
     );
   }
   

3fe6e93a4f04a107adf6f1ee64fcd8d1365fd49c
Jesus Herrera <jesusherrera1594@gmail.com>
2025-03-16 13:20:11 -0600

    initialize face detector and text recognition

---
+++ facedetector/android/app/src/main/kotlin/com/example/facedetector/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.facedetector
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ textrecognition/android/app/src/main/kotlin/com/example/textrecognition/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.textrecognition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
